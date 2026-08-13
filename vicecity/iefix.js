9cdf932ba311782ee16942ab3c37ce65789e3939
diddycated <anton3112z@gmail.com>
2026-08-13 22:17:29 +0200

    Upload 12

---
+++ vicecity/iefix.js
@@ -0,0 +1,43 @@
+function theDudeAbides(){
+	document.write('<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://active.macromedia.com/flash2/cabs/swflash.cab#version=6,0,0,0" ID=splash WIDTH=650 HEIGHT=400>');
+	document.write('<PARAM NAME=movie VALUE="mother.swf">');
+	document.write('<PARAM NAME=play VALUE=true>');
+	document.write('<PARAM NAME=loop VALUE=false>');
+	document.write('<PARAM NAME=quality VALUE=high>');
+	document.write('<PARAM NAME=scale VALUE="showall">');
+	document.write('<PARAM NAME=menu VALUE="FALSE">');
+	document.write('<EMBED src="mother.swf" type=application/x-shockwave-flash width="650" height="400" Loop="False" Quality="High" Menu="false" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"></embed>');
+	document.write('<NOEMBED><a href="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">you must download flash 6 to view this</a></NOEMBED>');
+	document.write('<NOSCRIPT><a href="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">you must download flash 6 to view this</a></NOSCRIPT>');
+	document.write('</OBJECT>');
+}
+
+function theDudeAbidesMain(){
+	document.write("<DIV ID=flashLayer STYLE=position:absolute;width:650px;height:400px;z-index:2;visibility:visible;background:#004F7B;border-width:1px;border-color:#000000;border-style:solid>");
+	document.write("<OBJECT classid=clsid:D27CDB6E-AE6D-11cf-96B8-444553540000 codebase=http://active.macromedia.com/flash2/cabs/swflash.cab#version=6,0,0,0 ID=splash WIDTH=650 HEIGHT=400>");
+	document.write("<PARAM NAME=movie VALUE=mother.swf>");
+	document.write("<PARAM NAME=play VALUE=true>");
+	document.write("<PARAM NAME=loop VALUE=false>");
+	document.write("<PARAM NAME=quality VALUE=high>");
+	document.write("<PARAM NAME=scale VALUE=showall>");
+	document.write("<PARAM NAME=menu VALUE=FALSE>");
+	document.write("<EMBED src=mother.swf type=application/x-shockwave-flash width=650 height=400 Loop=False Quality=High Menu=false pluginspage=http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash></embed>");
+	document.write("<NOEMBED><a href=http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash>you must download flash 6 to view this</a></NOEMBED>");
+	document.write("<NOSCRIPT><a href=http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash>you must download flash 6 to view this</a></NOSCRIPT>");
+	document.write("</OBJECT>");
+	document.write("</DIV>");
+}
+
+function theDudeAbidesMainTod(){
+	document.write('<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://active.macromedia.com/flash2/cabs/swflash.cab#version=6,0,0,0" ID=splash WIDTH=650 HEIGHT=400>');
+	document.write('<PARAM NAME=movie VALUE="mother_tod.swf">');
+	document.write('<PARAM NAME=play VALUE=true>');
+	document.write('<PARAM NAME=loop VALUE=false>');
+	document.write('<PARAM NAME=quality VALUE=high>');
+	document.write('<PARAM NAME=scale VALUE="showall">');
+	document.write('<PARAM NAME=menu VALUE="FALSE">');
+	document.write('<EMBED src="mother_tod.swf" type=application/x-shockwave-flash width="650" height="400" Loop="False" Quality="High" Menu="false" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">');
+	document.write('<NOEMBED><a href="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">you must download flash 6 to view this</a></NOEMBED>');
+	document.write('<NOSCRIPT><a href="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">you must download flash 6 to view this</a></NOSCRIPT>');
+	document.write('</OBJECT>');
+}
---
+++ vicecity/pc/iefix.js
@@ -0,0 +1,8 @@
+function theDudeAbides(){
+	document.write('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="650" height="500">');
+	document.write('<param name=movie value="pcvc.swf">');
+	document.write('<param name=menu value="false">');
+	document.write('<param name=quality value=high>');
+	document.write('<embed src="pcvc.swf" quality=high menu="false" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="650" height="500"></embed> ');
+	document.write('</object>');
+}
